f10629d0ebd66a43ec9814791d536a2bbb2b43be
Atrifex <rishirt.us@gmail.com>
2016-11-22 13:23:00 -0600

    Changed structure of png-to-txt

---
+++ PNG To Hex/On-Chip Memory/png_to_txt.py
@@ -0,0 +1,52 @@
+#
+# ECE385-HelperTools/PNG-To-Txt
+# Author: Rishi Thakkar
+#
+# NOTE: Please do not change the folder structure that has been provided or the tool will not work as intended.
+#
+
+from PIL import Image
+from collections import Counter
+from scipy.spatial import KDTree
+import numpy as np
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+import os
+
+welcomeString = "Welcome to PNG to Txt Convertor\nAuthor: Rishi Thakkar\n\nThis tool is developed as a collaborative effort of the ECE 385 staff.\n" +  bcolors.WARNING + "Don't forget to cite any tools that you use in your final project.\n" + bcolors.ENDC
+print(welcomeString)
+
+print(bcolors.HEADER + bcolors.BOLD + bcolors.UNDERLINE + "Modes:" + bcolors.ENDC)
+print(bcolors.OKGREEN + "PNG to Txt(Normal) - " + bcolors.ENDC + bcolors.WARNING + "Mode 0" + bcolors.ENDC)
+print(bcolors.OKGREEN + "PNG to 3 Txt(Channel Separation) - " + bcolors.ENDC + bcolors.WARNING + "Mode 1" + bcolors.ENDC)
+print(bcolors.OKGREEN + "PNG to Palette with resizing - " + bcolors.ENDC + bcolors.WARNING + "Mode 2" + bcolors.ENDC)
+print(bcolors.OKGREEN + "PNG to Palette with relative resizing - " + bcolors.ENDC + bcolors.WARNING + "Mode 3" + bcolors.ENDC)
+print("The best way to figure out which mode to use is to look at the detailed notes on https://github.com/Atrifex/ECE385-HelperTools.")
+
+print("Good luck and have fun with your projets!")
+
+while(1):
+    mode = int(input("Which mode of PNG to Txt would you like to use? Enter number between 0-3: "))
+    if mode == 0:
+        os.system("python3 ./scripts/png_to_txt.py")
+        quit()
+    elif mode == 1:
+        os.system("python3 ./scripts/png_to_3_txt.py")
+        quit()
+    elif mode == 2:
+        os.system("python3 ./scripts/png_to_palette_resizer.py")
+        quit()
+    elif mode == 3:
+        os.system("python3 ./scripts/png_to_palette_relative_resizer.py")
+        quit()
+    else:
+        print("Please enter a valid number.")
